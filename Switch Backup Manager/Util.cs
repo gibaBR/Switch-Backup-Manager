48d6a12c237200046d918db2ee67efeae83417c3
David Pierron <dpierron@gmail.com>
2018-09-07 20:27:18 +0200

    added a ImportedDate metadata ; also as a column in Local and EShop tables

--- Switch Backup Manager/Util.cs
+++ Switch Backup Manager/Util.cs
@@ -992,6 +992,7 @@ namespace Switch_Backup_Manager
                                    new XElement("ReleaseDate", data.ReleaseDate),
                                    new XElement("NumberOfPlayers", data.NumberOfPlayers),
                                    new XElement("ESRB", data.ESRB),
+                                   new XElement("ImportedDate", data.ImportedDate),
                                    new XElement("Categories", categories)
                            );
                         if (xml == LOCAL_FILES_DB)
@@ -1770,7 +1771,7 @@ namespace Switch_Backup_Manager
         public static FileData GetFileDataNSP(string file)
         {
             FileData data = new FileData();
-
+            data.ImportedDate = DateTime.UtcNow.ToString("yyyyMMddHHmmss");
             data.FilePath = file;
             data.FileName = Path.GetFileNameWithoutExtension(file);
             data.FileNameWithExt = Path.GetFileName(file);
@@ -2239,6 +2240,7 @@ namespace Switch_Backup_Manager
         public static FileData GetFileData(string filepath)
         {
             FileData result = new FileData();
+            result.ImportedDate = DateTime.UtcNow.ToString("yyyyMMddHHmmss");
             //Basic Info
             result.FilePath = filepath;
             result.FileName = Path.GetFileNameWithoutExtension(filepath);
@@ -2694,6 +2696,10 @@ namespace Switch_Backup_Manager
             try
             {
                 result.TitleID = xe.Attribute("TitleID").Value;
+                if (xe.Element("ImportedDate") != null)
+                {
+                    result.ImportedDate = xe.Element("ImportedDate").Value;
+                }
                 if (xe.Element("TitleIDBaseGame") != null)
                 {
                     result.TitleIDBaseGame = xe.Element("TitleIDBaseGame").Value;

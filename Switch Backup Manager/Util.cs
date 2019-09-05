49a5bf1bcf3ce5fef0f086e0ac841af90440d57e
Gilberto Antonio <gilberto.junior@gmail.com>
2019-09-05 20:19:26 -0300

    Update 1.2.2

--- Switch Backup Manager/Util.cs
+++ Switch Backup Manager/Util.cs
@@ -21,7 +21,7 @@ namespace Switch_Backup_Manager
 {
     internal static class Util
     {
-        public const string VERSION = "1.2.1";   //Actual application version
+        public const string VERSION = "1.2.2";   //Actual application version
         public const string MIN_DB_Version = "1.2.1"; //This is the minimum version of the DB that can work
 
         public const string INI_FILE = "sbm.ini";
